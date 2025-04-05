008b8cab69af0dcf03ef5b260a6c8951e17c6afc
Alejandro Duarte <87658315+AleDuarte20@users.noreply.github.com>
2025-04-04 22:46:05 -0300

    Update Settings.js

--- Settings.js
+++ Settings.js
@@ -22,11 +22,11 @@ class Setting {
         
         //default values
         settings.debug ? null : settings.debug = true
-        settings.proxy ? null : settings.proxy = {"server":"gate.smartproxy.com", "port":7000, "username":"user-sp54d7e3b9-country-fr", "password":"yfllka6nf4rc"}
+        settings.proxy ? null : settings.proxy = "your_proxy"
   
         settings.mongoDbName ? null : settings.mongoDbName = 'Apega'
         settings.mongoUri ? null : settings.mongoUri = `localhost:27017','Apega`
-        // settings.mongoUri ? null : settings.mongoUri = `mongodb+srv://admin:admin@leboncoin.r0gkf.mongodb.net/test?retryWrites=true&w=majority`
+        
         
         settings.max_search_pagination ? null : settings.max_search_pagination = '100'
   
